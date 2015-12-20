3c98be3c9df509bf89f023bbbf6a57145522d78d
Allen White <awhite23@rocketmail.com>
2015-12-20 16:41:52 -0500

    finished?

--- BYD ACT - SAT/LoginViewController.m
+++ BYD ACT - SAT/LoginViewController.m
@@ -9,6 +9,7 @@
 #import "LoginViewController.h"
 #import "WhichTestViewController.h"
 #import "UImodel.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
 
@@ -23,10 +24,19 @@
 
 @implementation LoginViewController
 
+NSString * const LOGIN_BOOL = @"userDefaultsLoginBool"; // const pointer
+NSString * const SEGUE_ID = @"loginTapped"; // const pointer
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 	self.navigationItem.title = @"Check in";
+	if ([[NSUserDefaults standardUserDefaults] boolForKey:LOGIN_BOOL]) {
+		[self performSegueWithIdentifier:SEGUE_ID sender:nil];
+	}
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,13 +49,21 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
-	if ([identifier isEqualToString:@"loginTapped"]) {
+	if ([identifier isEqualToString:SEGUE_ID]) {
 		NSString *name			= [self.nameTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
 		NSString *age			= [[[self.ageTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]] componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:@""];
 		NSString *grade			= [[[self.gradeTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]] componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:@""];
 		NSString *schoolName	= [self.schoolNameTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
 		NSString *email			= [self.emailTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
 		NSString *twitterOrIG		= [self.twitterOrIGTextField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
+		
+		NSLog(@"\n\n%@\n%@\n%@\n%@\n%@\n%@\n\n",
+		      name,
+		      age,
+		      grade,
+		      schoolName,
+		      email,
+		      twitterOrIG);
 		
 		if ([name isEqualToString:@""]) {
 			[UImodel showAlert:@"Whoops!" withMessage:@"Please fill out your name"];
@@ -64,6 +82,23 @@
 			return NO;
 		}
 		
+		[Parse setApplicationId:@"Kb3GFyrBfNLa2OEwIAmo26fSPUQw1mwHUZ5CjF8i"
+			      clientKey:@"Ix9HHnXltiqqiaqvtaBo6VXTkYSjA0ZMOsUiO6VF"];
+		
+		PFObject *testObject = [PFObject objectWithClassName:@"UserObject"];
+		
+		testObject[@"age"] = age;
+		testObject[@"email"] = email;
+		testObject[@"grade"] = grade;
+		testObject[@"name"] = name;
+		testObject[@"school"] = schoolName;
+		testObject[@"twitter_or_instagram"] = twitterOrIG;
+		
+		[testObject saveInBackground];
+		
+		[[NSUserDefaults standardUserDefaults] setBool:YES forKey:LOGIN_BOOL];
+		
+		return YES;
 		// dont need to set anything but we could?
 	}
 	return YES;
